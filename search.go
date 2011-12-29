019ffe767f3314b7351f9e2bd2b85cb792622bf8
James R. McKaskill <james@foobar.co.nz>
2011-12-29 15:57:14 -0500

    Rewrite to use LDAP protocol directly rather than ldap library

---
+++ search.go
@@ -0,0 +1,337 @@
+package ldap
+
+import (
+	"encoding/asn1"
+	"reflect"
+	"strconv"
+	"unicode"
+)
+
+type ObjectDN string
+
+var dnType = reflect.TypeOf(ObjectDN(""))
+
+type appendSlice struct {
+	val  reflect.Value
+	typ  reflect.Type
+	done chan error
+}
+
+func (s *appendSlice) onReply(tag int, msg []byte) status {
+	v := reflect.Indirect(reflect.New(s.typ))
+	sts, err := searchResult(tag, msg, v)
+
+	switch sts {
+	case finished:
+		s.done <- err
+	case data:
+		s.val.Set(reflect.Append(s.val, v))
+	}
+
+	return sts
+}
+
+type setStruct struct {
+	val      reflect.Value
+	done     chan error
+	haveData bool
+}
+
+func (s *setStruct) onReply(tag int, msg []byte) status {
+	sts, err := searchResult(tag, msg, s.val)
+
+	switch sts {
+	case finished:
+		if err == nil && !s.haveData {
+			err = ErrNotFound
+		}
+		s.done <- err
+	case data:
+		s.haveData = true
+	}
+
+	return sts
+}
+
+type callFunc struct {
+	fn   reflect.Value
+	typ  reflect.Type
+	done chan error
+}
+
+func (s *callFunc) onReply(tag int, msg []byte) status {
+	args := [1]reflect.Value{reflect.Indirect(reflect.New(s.typ))}
+	sts, err := searchResult(tag, msg, args[0])
+
+	switch sts {
+	case finished:
+		s.done <- err
+	case data:
+		rets := s.fn.Call(args[:])
+		if len(rets) > 0 {
+			if err, _ := rets[len(rets)-1].Interface().(error); err != nil {
+				s.done <- err
+				return abandon
+			}
+		}
+	}
+
+	return sts
+}
+
+type sendChan struct {
+	ch   reflect.Value
+	typ  reflect.Type
+	done chan error
+}
+
+func (s *sendChan) onReply(tag int, msg []byte) status {
+	v := reflect.Indirect(reflect.New(s.typ))
+	sts, err := searchResult(tag, msg, v)
+
+	switch sts {
+	case finished:
+		s.done <- err
+	case data:
+		s.ch.Send(v)
+	}
+
+	return sts
+}
+
+func searchResult(tag int, msg []byte, val reflect.Value) (status, error) {
+	switch tag {
+	case searchEntryTag:
+		if err := onSearchEntry(msg, val); err != nil {
+			return abandon, err
+		}
+		return data, nil
+
+	case extendedResultTag:
+		err := onSearchDone(msg, extendedResultParam)
+		return finished, err
+
+	case searchDoneTag:
+		err := onSearchDone(msg, searchDoneParam)
+		return finished, err
+	}
+
+	return ignored, nil
+}
+
+func onSearchDone(data []byte, param string) error {
+	res := result{}
+	if _, err := asn1.UnmarshalWithParams(data, &res, param); err != nil {
+		return err
+	}
+
+	if LdapResultCode(res.Code) != SuccessError {
+		return ErrLdap{&res}
+	}
+
+	return nil
+}
+
+func descEquals(desc []byte, name string) bool {
+	if len(name) > len(desc) {
+		return false
+	}
+
+	for i, b := range desc {
+		if unicode.ToUpper(rune(b)) != unicode.ToUpper(rune(desc[i])) {
+			return false
+		}
+	}
+
+	if len(desc) == len(name) {
+		return true
+	}
+
+	return desc[len(name)] == ';'
+}
+
+func getattr(e *searchEntry, name string) [][]byte {
+	for _, a := range e.Attrs {
+		if descEquals(a.Desc, name) {
+			return a.Vals
+		}
+	}
+
+	return nil
+}
+
+func onSearchEntry(data []byte, val reflect.Value) error {
+	e := searchEntry{}
+	if _, err := asn1.UnmarshalWithParams(data, &e, searchEntryParam); err != nil {
+		panic(err)
+		return err
+	}
+
+	typ := val.Type()
+	for i := 0; i < typ.NumField(); i++ {
+		ft := typ.Field(i)
+		fv := val.Field(i)
+
+		if ft.PkgPath != "" {
+			// private field
+			continue
+		}
+
+		if ft.Type == dnType {
+			fv.SetString(string(e.DN))
+			continue
+		}
+
+		vals := getattr(&e, ft.Name)
+		if len(vals) == 0 {
+			continue
+		}
+
+		switch ft.Type.Kind() {
+		case reflect.Slice, reflect.Array:
+			t := ft.Type.Elem()
+			switch t.Kind() {
+			case reflect.Slice, reflect.Array:
+				if t.Elem().Kind() == reflect.Uint8 {
+					// [][]byte
+					fv.Set(reflect.ValueOf(vals))
+				}
+
+			case reflect.Uint8:
+				// []byte
+				fv.Set(reflect.ValueOf(vals[0]))
+
+			case reflect.String:
+				// []string
+				strs := make([]string, len(vals))
+				buf := make([]byte, 0)
+				for _, v := range vals {
+					buf = append(buf, v...)
+				}
+				sbuf := string(buf)
+				for i, v := range vals {
+					strs[i] = sbuf[:len(v)]
+					sbuf = sbuf[len(v):]
+				}
+				fv.Set(reflect.ValueOf(strs))
+			}
+
+		case reflect.String:
+			fv.SetString(string(vals[0]))
+
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			v, err := strconv.ParseInt(string(vals[0]), 10, ft.Type.Bits())
+			if err != nil {
+				return err
+			}
+			fv.SetInt(v)
+
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			v, err := strconv.ParseUint(string(vals[0]), 10, ft.Type.Bits())
+			if err != nil {
+				return err
+			}
+			fv.SetUint(v)
+
+		case reflect.Float32, reflect.Float64:
+			v, err := strconv.ParseFloat(string(vals[0]), ft.Type.Bits())
+			if err != nil {
+				return err
+			}
+			fv.SetFloat(v)
+		}
+	}
+
+	return nil
+}
+
+func (db *DB) search(out interface{}, base string, filter Filter, scope asn1.Enumerated) error {
+	req := searchRequest{
+		BaseObject: []byte(base),
+		Scope:      scope,
+	}
+
+	var err error
+	if filter != nil {
+		req.Filter.FullBytes, err = filter.marshal()
+	} else {
+		req.Filter.FullBytes, err = Present("ObjectClass").marshal()
+	}
+
+	if err != nil {
+		return err
+	}
+
+	var typ reflect.Type
+	var reply replyHandler
+	done := make(chan error)
+
+	ov := reflect.ValueOf(out)
+	switch ov.Kind() {
+	case reflect.Ptr:
+		ov = reflect.Indirect(ov)
+
+		switch ov.Kind() {
+		case reflect.Slice:
+			typ = ov.Type().Elem()
+			reply = &appendSlice{ov, typ, done}
+		case reflect.Struct:
+			typ = ov.Type()
+			reply = &setStruct{ov, done, false}
+			req.SizeLimit = 1
+		}
+
+	case reflect.Func:
+		ft := ov.Type()
+		if ft.NumIn() != 1 {
+			panic("expected function with 1 argument")
+		}
+
+		typ = ft.In(1)
+		reply = &callFunc{ov, typ, done}
+
+	case reflect.Chan:
+		typ = ov.Type().Elem()
+		reply = &sendChan{ov, typ, done}
+
+	default:
+		return ErrUnsupportedType{ov.Type()}
+	}
+
+	if typ.Kind() != reflect.Struct {
+		return ErrUnsupportedType{typ}
+	}
+
+	for i := 0; i < typ.NumField(); i++ {
+		f := typ.Field(i)
+
+		if f.PkgPath != "" {
+			// private field
+			continue
+		}
+
+		if f.Type == dnType || len(f.Name) == 0 {
+			continue
+		}
+
+		req.Attrs = append(req.Attrs, []byte(f.Name))
+	}
+
+	if len(req.Attrs) == 0 {
+		req.Attrs = [][]byte{noAttributes}
+	}
+
+	if err := db.send(searchRequestParam, req, reply); err != nil {
+		return err
+	}
+
+	return <-done
+}
+
+func (db *DB) SearchTree(out interface{}, base string, filter Filter) error {
+	return db.search(out, base, filter, wholeSubtreeScope)
+}
+
+func (db *DB) GetObject(out interface{}, dn string) error {
+	return db.search(out, dn, nil, baseObjectScope)
+}
---
+++ sid.go
@@ -0,0 +1,73 @@
+package ldap
+
+import (
+	"encoding/binary"
+	"strconv"
+	"strings"
+)
+
+type SID []byte
+
+const sidRevision = 1
+
+func ParseSID(s string) (SID, error) {
+	if !strings.HasPrefix(s, "S-1-") {
+		return nil, ErrInvalidSID
+	}
+
+	parts := strings.Split(s[len("S-1-"):], "-")
+	sid := make([]byte, 4+4*len(parts))
+
+	if len(parts) < 1 {
+		return nil, ErrInvalidSID
+	}
+
+	v, err := strconv.ParseUint(parts[0], 10, 48)
+	if err != nil {
+		return nil, ErrInvalidSID
+	}
+
+	binary.BigEndian.PutUint64(sid, v)
+	sid[0] = sidRevision
+	sid[1] = byte(len(parts) - 1)
+
+	for i, p := range parts[1:] {
+		v, err := strconv.ParseUint(p, 10, 32)
+		if err != nil {
+			return nil, ErrInvalidSID
+		}
+		binary.LittleEndian.PutUint32(sid[8+i*4:], uint32(v))
+	}
+
+	return sid, nil
+}
+
+func (s SID) String() string {
+	if len(s) < 8 || s[0] != sidRevision || len(s) != (int(s[1])*4)+8 {
+		return ""
+	}
+
+	ret := []byte("S-1-")
+	ret = strconv.AppendUint(ret, binary.BigEndian.Uint64(s[:8])&0xFFFFFFFFFFFF, 10)
+
+	for i := 0; i < int(s[1]); i++ {
+		ret = append(ret, "-"...)
+		ret = strconv.AppendUint(ret, uint64(binary.LittleEndian.Uint32(s[8+i*4:])), 10)
+	}
+
+	return string(ret)
+}
+
+func (s SID) Equal(r SID) bool {
+	if len(s) != len(r) {
+		return false
+	}
+
+	for i, a := range s {
+		if a != r[i] {
+			return false
+		}
+	}
+
+	return true
+}
